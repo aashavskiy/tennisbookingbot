235b1dd48227177062e14c1c955c8fcb94d7ea20
aashavskiy <ashavskiy@gmail.com>
2025-02-23 13:06:23 +0200

    1

--- tennis_booking_bot.py
+++ tennis_booking_bot.py
@@ -129,4 +129,5 @@ if __name__ == "__main__":
     init_db()
     bot.remove_webhook()
     bot.set_webhook(url=f"{WEBHOOK_URL}/{TOKEN}")
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
